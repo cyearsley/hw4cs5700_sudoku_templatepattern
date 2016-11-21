f83f80992c15a38acb830a9db3fdeb51fb9a0e53
Caleb Yearsley <cnyearsley@outlook.com>
2016-11-21 02:36:36 -0700

    Added smaller puzzles, syntax checking for puzzles, and format checking for puzzles

--- hw4_sudoku/sudokuAlgorithm.cs
+++ hw4_sudoku/sudokuAlgorithm.cs
@@ -2,16 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace sudoku_algorithms
 {
     public abstract class sudokuAlgorithm
     {
-        int numberOfSolutions;
-        private List<List<int>> solvedPuzzle;
-        private List<List<int>> rawPuzzle;
-        private String puzzleFile;
+        protected int numberOfSolutions;
+        protected List<List<int>> solvedPuzzle;
+        protected List<List<int>> rawPuzzle;
+        protected String puzzleFile;
         protected abstract void executeSudokuAlgorithm(List<List<int>> puzzle);
 
         public sudokuAlgorithm()
@@ -42,7 +43,84 @@ namespace sudoku_algorithms
 
         private bool checkFormat()
         {
-            // TODO: read the specified file to ensure that the puzzle structure is valid using puzzleFile.
+            string fileContentsString = System.IO.File.ReadAllText(@puzzleFile);
+            Regex regexp = new Regex(@"[^1-9\-\s]*", RegexOptions.Singleline);
+            MatchCollection matches = regexp.Matches(fileContentsString);
+            List<String> errorList = new List<String>();
+            for (int ii = 0; ii < matches.Count; ii++)
+            {
+                if (matches[ii].ToString() != "" && matches[ii].ToString() != null)
+                {
+                    errorList.Add("Invalid Syntax: " + matches[ii].ToString());
+                }
+            }
+
+            int[] acceptedSizes = { 4, 9, 16, 25, 36 };
+            string[] fileContentsArray = System.IO.File.ReadAllLines(@puzzleFile);
+            if (!acceptedSizes.Contains(fileContentsArray.Length))
+            {
+                errorList.Add("Invalid Format - Wrong number of rows!\n "+
+                    "Given: " + fileContentsArray.Length + "Expected: 4, 9, 16, 25, or 36.");
+            }
+
+            for (int ii = 0; ii < fileContentsArray.Length; ii++)
+            {
+                int columnCount = fileContentsArray[ii].Split(' ').Length;
+                if (!acceptedSizes.Contains(columnCount))
+                {
+                    errorList.Add("Invalid Format - Wrong number of columns!\n " +
+                        "Given: " + columnCount + ". Expected: 4, 9, 16, 25, or 36.");
+                }
+                if (columnCount != fileContentsArray.Length)
+                {
+                    errorList.Add("The number of columns do not match the number of rows!\n" +
+                        "Given: " + columnCount + ". Expected: " + fileContentsArray.Length);
+                }
+            }
+
+            if (errorList.Count > 0)
+            {
+                Console.WriteLine("\nThere was a problem reading your puzzle, here are the errors found:");
+                for (int ii = 0; ii < errorList.Count; ii++)
+                {
+                    Console.WriteLine(ii + 1 + ") " + errorList[ii] + "\n");
+                }
+                return false;
+            }
+
+            Console.WriteLine("Solving the following puzzle: ");
+
+            rawPuzzle = new List<List<int>>();
+            List<List<String>> puzzleTableString = new List<List<String>>();
+            for (int ii = 0; ii < fileContentsArray.Length; ii++)
+            {
+                puzzleTableString.Add(fileContentsArray[ii].Split(' ').ToList());
+            }
+
+            for (int ii = 0; ii < puzzleTableString.Count; ii++)
+            {
+                rawPuzzle.Add(new List<int>());
+                for (int jj = 0; jj < puzzleTableString.Count; jj++)
+                {
+                    if (puzzleTableString[ii][jj] == "-")
+                    {
+                        rawPuzzle[ii].Add(0);
+                    }
+                    else
+                    {
+                        rawPuzzle[ii].Add(Int32.Parse(puzzleTableString[ii][jj]));
+                    }
+                }
+            }
+            Console.WriteLine("The count of List[0] is: " + puzzleTableString[0].Count);
+            for (int ii = 0; ii < rawPuzzle[0].Count; ii++)
+            {
+                for (int jj = 0; jj < rawPuzzle[0].Count; jj++)
+                {
+                    Console.Write(rawPuzzle[ii][jj] + " ");
+                }
+                Console.WriteLine("\n");
+            }
             return true;
         }
 
@@ -52,7 +130,7 @@ namespace sudoku_algorithms
             this.rawPuzzle = new List<List<int>>();
         }
 
-        private void print()
+        protected void print()
         {
             Console.WriteLine("print the output to a file");
             if (numberOfSolutions == 1)
@@ -69,7 +147,20 @@ namespace sudoku_algorithms
 
         private bool getFileName()
         {
-            this.puzzleFile = "testing.txt";
+            Console.WriteLine("Enter the file name that contains the sudoku puzzle.");
+            Console.Write("Enter: ");
+            String userInput = Console.ReadLine();
+
+            try
+            {
+                string[] fileContents = System.IO.File.ReadAllLines(@"../../" + userInput);
+            }
+            catch(Exception exp)
+            {
+                return false;
+            }
+
+            this.puzzleFile = "../../" + userInput;
             return true;
         }
     }
--- hw4_sudoku/sudokuNaiveDancingLinksAlgorithm.cs
+++ hw4_sudoku/sudokuNaiveDancingLinksAlgorithm.cs
@@ -8,9 +8,107 @@ namespace sudoku_algorithms
 {
     class sudokuNaiveDancingLinksAlgorithm : sudokuAlgorithm
     {
-        protected override void executeSudokuAlgorithm(List<List<int>> puzzle)
+        protected override void executeSudokuAlgorithm(List<List<int>> sudoku)
         {
-
+            solve(sudoku, 0);
         }
+
+        // Start Reference: https://rafal.io/posts/solving-sudoku-with-dancing-links.html
+        // ============================================================================== //
+        private void solve(List<List<int>> sudoku, int idx)
+        {
+            int size = sudoku[0].Count;
+            if (idx == size * size)
+            {
+                if (isSolution(sudoku))
+                {
+                    Console.WriteLine("Found a solution via very naive algorithm: ");
+                    //print(sudoku);
+                    print();
+                    Console.WriteLine("\n");
+                }
+            }
+            else
+            {
+                int row = idx / size;
+                int col = idx % size;
+                if (sudoku[row][col] != 0)
+                { // the square is already filled in, don't do anything 
+                    solve(sudoku, idx + 1);
+                }
+                else
+                {
+                    for (int i = 1; i <= size; i++)
+                    {
+                        sudoku[row][col] = i;
+                        solve(sudoku, idx + 1); // continue with the next square
+                        sudoku[row][col] = 0; // unmake move
+                    }
+                }
+            }
+        }
+        
+        // Returns true if and only if sudoku is a valid solved sudoku board
+        private bool isSolution(List<List<int>> sudoku)
+        {
+            int N = rawPuzzle[0].Count;
+            int side = (int)Math.Floor(Math.Sqrt(rawPuzzle[0].Count));
+            bool[] mask = new bool[N + 1];
+
+            // Check rows
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    int num = sudoku[i][j];
+                    if (mask[num]) return false;
+                    mask[num] = true;
+                }
+                for (int ii = 0; ii < mask.Count(); ii++)
+                {
+                    mask[ii] = false;
+                }
+            }
+
+            // Check columns
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    int num = sudoku[j][i];
+                    if (mask[num]) return false;
+                    mask[num] = true;
+                }
+                for (int ii = 0; ii < mask.Count(); ii++)
+                {
+                    mask[ii] = false;
+                }
+            }
+
+            // Check subsquares
+
+            for (int i = 0; i < N; i += side)
+            {
+                for (int j = 0; j < N; j += side)
+                {
+                    for (int ii = 0; ii < mask.Count(); ii++)
+                    {
+                        mask[ii] = false;
+                    }
+                    for (int di = 0; di < side; di++)
+                    {
+                        for (int dj = 0; dj < side; dj++)
+                        {
+                            int num = sudoku[i + di][j + dj];
+                            if (mask[num]) return false;
+                        }
+                    }
+                }
+            }
+
+            return true; // Everything validated!
+        }
+        // ============================================================================== //
+        // End Reference
     }
 }
